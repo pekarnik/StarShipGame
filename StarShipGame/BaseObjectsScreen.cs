4392960189c85e0b17d8df2000271a1a333e2241
Pekarnik <pekgoglo@gmail.com>
2018-08-01 20:04:49 +0300

    BaseObjectforScreen(Just task)

---
+++ StarShipGame/BaseObjectsScreen.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing;
+namespace StarShipGame
+{
+    class BaseObjectScreen
+    {
+        protected Point pos;
+        protected Point dir;
+        protected Size size;
+        public BaseObjectScreen(Point _pos, Point _dir, Size _size)
+        {
+            pos = _pos;
+            dir = _dir;
+            size = _size;
+        }       
+        private static Image image = Image.FromFile("image.png");
+        public virtual void Draw()
+        {
+            
+            SplashScreen.buffer.Graphics.DrawImage(image, pos.X, pos.Y, size.Width, size.Height);
+        }
+        public virtual void Update()
+        {
+            pos.X = pos.X - dir.X;
+            pos.Y += dir.Y;
+            if (pos.X < 0 || pos.X > Game.Width) dir.X = -dir.X;
+            if (pos.Y < 0 || pos.Y > Game.Height) dir.Y = -dir.Y;
+            
+        }
+    }
+}
