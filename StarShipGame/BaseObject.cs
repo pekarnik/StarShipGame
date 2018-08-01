9d4e86dd1982abc13088a6eb3c035fb59290e5cc
Pekarnik <pekgoglo@gmail.com>
2018-08-01 20:04:17 +0300

    BaseObject(Images and a Base class)

---
+++ StarShipGame/BaseObject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+namespace StarShipGame
+{
+    class BaseObject
+    {
+        protected Point pos;
+        protected Point dir;
+        protected Size size;
+        public BaseObject(Point _pos, Point _dir, Size _size)
+        {
+            pos = _pos;
+            dir = _dir;
+            size = _size;
+        }
+        private static Image image = Image.FromFile("image.png");
+        public virtual void Draw()
+        {            
+            Game.buffer.Graphics.DrawImage(image, pos.X, pos.Y, size.Width, size.Height);
+        }
+        public virtual void Update()
+        {
+            pos.X = pos.X - dir.X;
+            pos.Y += dir.Y;
+            if (pos.X < 0 || pos.X > Game.Width) dir.X = -dir.X;
+            if (pos.Y < 0 || pos.Y > Game.Height) dir.Y = -dir.Y;
+        }
+    }
+}
