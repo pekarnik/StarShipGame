ebf0b26993f367b526eab1fccdd37477ea0cb786
Pekarnik <pekgoglo@gmail.com>
2018-08-06 19:29:16 +0300

    пуля

---
+++ StarShipGame/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Drawing;
+
+namespace StarShipGame
+{
+    class Bullet : BaseObject
+    {
+        /// <summary>
+        /// Вызываем конструктор из BaseObject
+        /// </summary>
+        /// <param name="_pos"></param>
+        /// <param name="_dir"></param>
+        /// <param name="_size"></param>
+        public Bullet(Point _pos, Point _dir, Size _size) : base(_pos, _dir,_size, "bullet.png")
+        {
+
+        }
+        /// <summary>
+        /// Движение объекта
+        /// </summary>
+        public override void Update()
+        {
+            Random rnd = new Random();
+            pos.X+= dir.X;                   
+        }        
+    }
+}
